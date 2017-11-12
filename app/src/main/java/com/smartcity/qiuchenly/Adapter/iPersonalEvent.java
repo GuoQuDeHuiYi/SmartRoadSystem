33c5eac9468537baf6ce075e2e49d83d331fac4c
GuoQuDeHuiYi <32737179+GuoQuDeHuiYi@users.noreply.github.com>
2017-11-12 20:30:37 +0800

    添加个人中心的ViewPager，增加了三个布局 (#3)

---
+++ app/src/main/java/com/smartcity/qiuchenly/Adapter/iPersonalEvent.java
@@ -0,0 +1,11 @@
+package com.smartcity.qiuchenly.Adapter;
+
+import android.view.View;
+
+/**
+ * Created by Eason on 2017/11/12.
+ */
+
+public interface iPersonalEvent {
+    void PersonsetViewEvent(View v,int p);
+}
