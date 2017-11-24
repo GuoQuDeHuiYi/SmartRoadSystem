1444123b68e06b92c67d9f069c4c92800e457a3a
QiuChenly <QiuChenly@users.noreply.github.com>
2017-11-24 17:14:14 +0800

    add some VirtualData

--- app/src/main/java/com/smartcity/qiuchenly/VirtualData/userManageDataBase.java
+++ app/src/main/java/com/smartcity/qiuchenly/VirtualData/userManageDataBase.java
@@ -10,71 +10,76 @@ package com.smartcity.qiuchenly.VirtualData;
  */
 
 public class userManageDataBase {
-    public userManageDataBase() {
+  public userManageDataBase() {
 
+  }
+
+  public static String getUsers() {
+    return "{\n" +
+            "            \"data\": [\n" +
+            "                {\n" +
+            "                    \"carID\": \"辽A10001\",\n" +
+            "                    \"carMaster\": \"张三\",\n" +
+            "                    \"totalMoney\": 100,\n" +
+            "                    \"carTypePic\": 1\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"carID\": \"辽A10002\",\n" +
+            "                    \"carMaster\": \"李四\",\n" +
+            "                    \"totalMoney\": 99,\n" +
+            "                    \"carTypePic\": 2\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"carID\": \"辽A10003\",\n" +
+            "                    \"carMaster\": \"王五\",\n" +
+            "                    \"totalMoney\": 103,\n" +
+            "                    \"carTypePic\": 3\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"carID\": \"辽A10004\",\n" +
+            "                    \"carMaster\": \"赵六\",\n" +
+            "                    \"totalMoney\": 1,\n" +
+            "                    \"carTypePic\": 4\n" +
+            "                }" +
+            "            ]\n" +
+            "        }";
+  }
+
+  public static String VirtualWebSiteLogin(String user, String pass) {
+    //defined five users by default
+    String users[] = new String[]{
+            "user1", "user2", "user3", "user4", "user5"
+    };
+    //defined default userPass
+    String defaultPass = "123456";
+
+    boolean isHave = false;
+    int count = 0;
+    //start verify user is have?
+    for (String user_Temp : users) {
+      count++;
+      if (user_Temp.equals(user)) {
+        isHave = true;//userName is valid!
+        break;
+      }
     }
-
-    public static String getUsers() {
-        return "{\n" +
-                "            \"data\": [\n" +
-                "                {\n" +
-                "                    \"carID\": \"辽A10001\",\n" +
-                "                    \"carMaster\": \"张三\",\n" +
-                "                    \"totalMoney\": 100,\n" +
-                "                    \"carTypePic\": 1\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"carID\": \"辽A10002\",\n" +
-                "                    \"carMaster\": \"李四\",\n" +
-                "                    \"totalMoney\": 99,\n" +
-                "                    \"carTypePic\": 2\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"carID\": \"辽A10003\",\n" +
-                "                    \"carMaster\": \"王五\",\n" +
-                "                    \"totalMoney\": 103,\n" +
-                "                    \"carTypePic\": 3\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"carID\": \"辽A10004\",\n" +
-                "                    \"carMaster\": \"赵六\",\n" +
-                "                    \"totalMoney\": 1,\n" +
-                "                    \"carTypePic\": 4\n" +
-                "                }"+
-                "            ]\n" +
-                "        }";
+    if (!isHave) {
+      return "{\"loginState\":\"-1\",\"errNo\":\"1000\",\"errReason\":\"userName is unValid!\"}";
     }
-
-    public static String VirtualWebSiteLogin(String user, String pass) {
-        //defined five users by default
-        String users[] = new String[]{
-                "user1", "user2", "user3", "user4", "user5"
-        };
-        //defined default userPass
-        String defaultPass = "123456";
-
-        boolean isHave = false;
-        int count = 0;
-        //start verify user is have?
-        for (String user_Temp : users) {
-            count++;
-            if (user_Temp.equals(user)) {
-                isHave = true;//userName is valid!
-                break;
-            }
-        }
-        if (!isHave) {
-            return "{\"loginState\":\"-1\",\"errNo\":\"1000\",\"errReason\":\"userName is unValid!\"}";
-        }
-        //if passWord is Error.
-        if (!defaultPass.equals(pass)) {
-            return "{\"loginState\":\"-1\",\"errNo\":\"1001\",\"errReason\":\"passWord is error!\"}";
-        }
-        //now,this account is valid,so we now must return this account Relevant Information
-        boolean VIPLevel = false;
-        if (count == 1) {
-            VIPLevel = true;
-        }
-        return "{\"loginState\":\"0\",\"errNo\":\"200\",\"errReason\":\"\",\"isVIP\":\"" + (VIPLevel ? "1" : "0") + "\"}";
+    //if passWord is Error.
+    if (!defaultPass.equals(pass)) {
+      return "{\"loginState\":\"-1\",\"errNo\":\"1001\",\"errReason\":\"passWord is error!\"}";
     }
+    //now,this account is valid,so we now must return this account Relevant Information
+    boolean VIPLevel = false;
+    if (count == 1) {
+      VIPLevel = true;
+    }
+    //sex 1 = 男 2 = 女
+    return "{\"loginState\":\"0\",\"cardID\":\"3209241999999999\",\"sex\":\"1\"," +
+            "\"errNo\":\"200\"," + "\"userName\":\"" + users[count - 1] + "\"," +
+            "\"errReason\":\"\"," + "\"phoneNum\":\"18999999999\"," +
+            "\"regTime\":\"2017.5.1\"," +
+            "\"isVIP\":\"" + (VIPLevel ? "1" : "0") + "\"}";
+  }
 }
